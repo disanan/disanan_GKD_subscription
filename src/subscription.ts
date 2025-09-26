2f38ffc8ada5d0156435d70031c71ec9e355af5c
第三岸 <370188227@qq.com>
2025-09-26 18:51:43 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 2025,
   name: 'Subscription',
   version: 0,
-  author: 'author',
+  author: 'disanan',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
