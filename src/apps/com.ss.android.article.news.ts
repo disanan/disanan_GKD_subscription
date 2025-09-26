43364472fc3ede7b9bc43af7269604af51f3039d
第三岸 <370188227@qq.com>
2025-09-26 19:12:04 +0800

    Update com.ss.android.article.news.ts

--- src/apps/com.ss.android.article.news.ts
+++ src/apps/com.ss.android.article.news.ts
@@ -1,1 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
 
+export default defineGkdApp({
+  id: 'com.ss.android.article.news',
+  name: '今日头条',
+  groups: [
+    {
+      key: 1,
+      name: '今日头条-暂不关注',
+      fastQuery: true,
+      actionDelay:200,
+      actionCd:5000,
+      activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
+      rules: [
+        {
+	        matches: ['[id="com.ss.android.article.news:id/l4k"]','[text="暂不关注"]'],
+          snapshotUrls: 'https://i.gkd.li/i/22505380',//获取链接的方法在上一步有说明
+        },
+       ],
+    }
+  ],
+});
