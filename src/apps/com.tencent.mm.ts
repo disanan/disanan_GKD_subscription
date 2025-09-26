32875c74475dda88f4d368a5bc3ea83750eea153
第三岸 <370188227@qq.com>
2025-09-26 18:34:54 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,5 +3,21 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+    {
+      key: 1,
+      name: '用腾讯文件打开-允许',
+      fastQuery: true,
+      actionMaximum:1,
+      actionDelay:200,
+      actionCd:5000,
+      activityIds: ['.ui.chatting.AppAttachNewDownloadUI'],
+      rules: [
+        {
+		      matches: ['[id="com.tencent.mm:id/mm_alert_ok_btn"]','[text="允许"]'],
+          snapshotUrls: 'https://i.gkd.li/i/22210190',//获取链接的方法在上一步有说明
+        },
+       ],
+    }
+  ],
 });
