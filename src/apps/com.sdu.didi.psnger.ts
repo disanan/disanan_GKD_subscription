e403a67b1e0e5ddf91a2327cf703c551aa265d41
第三岸 <370188227@qq.com>
2025-09-28 09:03:59 +0800

    Update com.sdu.didi.psnger.ts

--- src/apps/com.sdu.didi.psnger.ts
+++ src/apps/com.sdu.didi.psnger.ts
@@ -15,6 +15,7 @@ export default defineGkdApp({
           activityIds: 'com.didi.ride.dimina.DMActivity',
           matches:
             '@Image <2 View < View <4 View < WebView < WebView < [vid="touch_intercept_fl"]',
+            '@Image <2 View < View < View <5 View < View < WebView < WebView < [vid="touch_intercept_fl"]',
         },
       ],
     },
@@ -27,7 +28,12 @@ export default defineGkdApp({
         {
           fastQuery: true,
           activityIds: 'com.didi.sdk.app.MainActivity',
-          matches: '[id="com.sdu.didi.psnger:id/popClose"][desc="关闭弹窗"]',
+          matches:
+            '[id="com.sdu.didi.psnger:id/popClose"][desc="关闭弹窗"]',
+            '[id="com.sdu.didi.psnger:id/iv_close"]',
+            '@TextView <2 View < View <2 View <6 WebView < WebView < FrameLayout < [vid="web_parent"]',
+            '@TextView <2 View <2 View <2 View < View < WebView < WebView < FrameLayout < [vid="web_parent"]',
+            '@ImageView < ViewGroup <4 ViewGroup <5 ViewGroup <3 ViewGroup < ViewGroup <3 ViewGroup < [vid="hummer_layout_root_view"]',
         },
       ],
     },
@@ -44,47 +50,5 @@ export default defineGkdApp({
         },
       ],
     },
-    {
-      key: 4,
-      name: '马上领',
-      actionDelay: 100,
-      actionCd: 3000,
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.didi.sdk.app.MainActivity',
-          matches:
-            '@ImageView < ViewGroup <4 ViewGroup <5 ViewGroup <3 ViewGroup < ViewGroup <3 ViewGroup < [vid="hummer_layout_root_view"]',
-        },
-      ],
-    },
-    {
-      key: 5,
-      name: '全部收下',
-      actionDelay: 100,
-      actionCd: 3000,
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.didi.sdk.app.MainActivity',
-          matches:
-            '@TextView <2 View < View <2 View <6 WebView < WebView < FrameLayout < [vid="web_parent"]',
-        },
-      ],
-    },
-    {
-      key: 6,
-      name: '弹窗6',
-      actionDelay: 100,
-      actionCd: 3000,
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.didi.sdk.app.MainActivity',
-          matches:
-            '@TextView <2 View <2 View <2 View < View < WebView < WebView < FrameLayout < [vid="web_parent"]',
-        },
-      ],
-    },
-  ],
+   ],
 });
