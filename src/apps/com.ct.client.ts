eb4604b043c03018de690dc53141e33c038ef406
第三岸 <370188227@qq.com>
2025-10-16 12:46:26 +0800

    Update com.ct.client.ts

--- src/apps/com.ct.client.ts
+++ src/apps/com.ct.client.ts
@@ -1,1 +1,30 @@
+import { defineGkdApp } from '@gkd-kit/define';
 
+export default defineGkdApp({
+  id: 'com.ct.client',
+  name: '中国电信',
+  groups: [
+    {
+      key: 1,
+      name: '关闭广告',
+      fastQuery: true,
+      actionDelay: 200,
+      actionCd: 1000,
+      activityIds: ['.activity.MainActivity'],
+      rules: [
+        {
+          matches: [
+            '[id="com.ct.client:id/cbNoMore"]',
+            '[text="不再弹出"]',
+          ],
+          actionDelay: 200,
+          matches: [
+            '[id="com.ct.client:id/close"]',
+            '[vid="close"]',
+          ],
+          
+        },
+      ],
+    },
+  ],
+});
